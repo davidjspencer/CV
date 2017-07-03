34873877a5840c0b0a8c6dce92cfd69a2dc756ba
davidjspencer <davidjspencer@gmail.com>
2017-07-03 11:40:55 +0200

    2 colour options

--- js/change-color.js
+++ js/change-color.js
@@ -1,15 +1,19 @@
 (function(){
   var button = document.getElementById("change");
   var page = document.getElementById('change-grid-container');
-  
+
 	button.addEventListener("click", changeColor);
 
 	function changeColor() {
-    if (page.classList.contains('color-change')) {
-      console.log('contains color-change')
+    if (page.classList.contains('color-change') || page.classList.contains('color-change1')) {
       page.classList.remove('color-change');
+      page.classList.remove('color-change1');
     } else {
-      page.classList.add('color-change');
+        if (Math.random() >= 0.5) {
+        page.classList.add('color-change');
+      } else {
+        page.classList.add('color-change1');
+      }
     }
 	}
 })();
