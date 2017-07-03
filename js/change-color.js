9336516ae69191cb803c92f238658925ec0e9ea7
davidjspencer <davidjspencer@gmail.com>
2017-07-03 12:27:40 +0200

    3 options sequential

--- js/change-color.js
+++ js/change-color.js
@@ -3,17 +3,37 @@
   var page = document.getElementById('change-grid-container');
 
 	button.addEventListener("click", changeColor);
+  // Random colour changer
+	/*
+  function changeColor() {
+     if (page.classList.contains('color-change') || page.classList.contains('color-change1')) {
+       page.classList.remove('color-change');
+       page.classList.remove('color-change1');
+     } else {
+         if (Math.random() >= 0.5) {
+         page.classList.add('color-change');
+       } else {
+         page.classList.add('color-change1');
+       }
+     }
+	 }
+   */
 
-	function changeColor() {
-    if (page.classList.contains('color-change') || page.classList.contains('color-change1')) {
-      page.classList.remove('color-change');
-      page.classList.remove('color-change1');
+  // Sequential colour changer
+  console.log('before function fired');
+  function changeColor() {
+    if (!(page.classList.contains('color-change') || page.classList.contains('color-change1') || page.classList.contains('color-change2'))) {
+      page.classList.add('color-change');
     } else {
-        if (Math.random() >= 0.5) {
-        page.classList.add('color-change');
-      } else {
-        page.classList.add('color-change1');
+        if (page.classList.contains('color-change')) {
+          page.classList.remove('color-change');
+          page.classList.add('color-change1');
+        } else if (page.classList.contains('color-change1')) {
+          page.classList.remove('color-change1');
+          page.classList.add('color-change2');
+        } else {
+          page.classList.remove('color-change2');
+        }
       }
     }
-	}
 })();
